f3430267e39471d40a31b436c21f25f21b89cf02
Camila Pessoni <pessoni.camila.cp@gmail.com>
2023-09-11 22:01:52 -0300

    aula03

---
+++ aula_03/src/components/PrimeiroComp.jsx
@@ -0,0 +1,8 @@
+export default function PrimeiroComponente(props){
+    return (
+        <>
+            <h1 className={props.css}>{props.titulo}</h1>
+            <h2>{props.subtitulo}</h2>
+        </>
+    )
+}
